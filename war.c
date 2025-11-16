c256290a54df200fbd090bfb85eea1ce4a10e143
elianatercetti <elianatercetti123@gmail.com>
2025-11-16 00:58:03 +0000

    extrutura de dados aventureiro

--- war.c
+++ war.c
@@ -15,48 +15,109 @@
 // ============================================================================
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-// Estrutura (struct) que guarda as informações de um território
+
 struct Territorio {
-    char nome[30];   // nome do território
-    char cor[10];    // cor do exército
-    int tropas;      // quantidade de tropas
+    char nome[30];
+    char cor[10];
+    int tropas;
 };
 
+
+void cadastrarTerritorios(struct Territorio *mapa, int qtd) {
+    int i;
+    for (i = 0; i < qtd; i++) {
+        printf("\n--- Cadastro do territorio %d ---\n", i + 1);
+        printf("Nome: ");
+        scanf("%s", mapa[i].nome);
+
+        printf("Cor do exercito: ");
+        scanf("%s", mapa[i].cor);
+
+        printf("Quantidade de tropas: ");
+        scanf("%d", &mapa[i].tropas);
+    }
+}
+
+
+void exibirTerritorios(struct Territorio *mapa, int qtd) {
+    int i;
+    printf("\n===== TERRITORIOS ATUAIS =====\n");
+    for (i = 0; i < qtd; i++) {
+        printf("%d - Nome: %s | Cor: %s | Tropas: %d\n", i + 1, mapa[i].nome, mapa[i].cor, mapa[i].tropas);
+    }
+}
+
+
+void atacar(struct Territorio *atacante, struct Territorio *defensor) {
+    
+    int dadoAtacante = (rand() % 6) + 1;
+    int dadoDefensor = (rand() % 6) + 1;
+
+    printf("\n--- ATAQUE EM ANDAMENTO ---\n");
+    printf("%s (cor %s) rolou: %d\n", atacante->nome, atacante->cor, dadoAtacante);
+    printf("%s (cor %s) rolou: %d\n", defensor->nome, defensor->cor, dadoDefensor);
+
+    
+    if (dadoAtacante > dadoDefensor) {
+        printf("O atacante venceu a batalha!\n");
+
+        
+        strcpy(defensor->cor, atacante->cor);
+        defensor->tropas = atacante->tropas / 2; 
+        atacante->tropas = atacante->tropas / 2; 
+
+    } else {
+        printf("O defensor resistiu ao ataque!\n");
+        atacante->tropas -= 1; 
+    }
+}
+
+
+void liberarMemoria(struct Territorio *mapa) {
+    free(mapa);
+    printf("\nMemoria liberada com sucesso!\n");
+}
+
 int main() {
+    srand(time(NULL)); 
 
-    // Cria um vetor com 5 territórios
-    struct Territorio territorios[5];
+    int qtd;
+    printf("Digite quantos territorios voce quer cadastrar: ");
+    scanf("%d", &qtd);
 
-    int i; // variável para o laço
+    struct Territorio *mapa = (struct Territorio*) malloc(qtd * sizeof(struct Territorio));
 
-    printf("===== CADASTRO DE TERRITORIOS =====\n\n");
-
-    // Cadastro dos 5 territórios
-    for (i = 0; i < 5; i++) {
-        printf("Digite o nome do territorio %d: ", i + 1);
-        scanf("%s", territorios[i].nome);
-
-        printf("Digite a cor do exercito do territorio %d: ", i + 1);
-        scanf("%s", territorios[i].cor);
-
-        printf("Digite a quantidade de tropas do territorio %d: ", i + 1);
-        scanf("%d", &territorios[i].tropas);
-
-        printf("\n"); // pular linha entre cadastros
+    if (mapa == NULL) {
+        printf("Erro ao alocar memoria!\n");
+        return 1;
     }
 
-    // Exibição dos territórios cadastrados
-    printf("===== LISTA DE TERRITORIOS CADASTRADOS =====\n\n");
+    cadastrarTerritorios(mapa, qtd);
 
-    for (i = 0; i < 5; i++) {
-        printf("Territorio %d:\n", i + 1);
-        printf("Nome: %s\n", territorios[i].nome);
-        printf("Cor do Exercito: %s\n", territorios[i].cor);
-        printf("Quantidade de Tropas: %d\n", territorios[i].tropas);
-        printf("------------------------------\n");
+    exibirTerritorios(mapa, qtd);
+
+    int a, d;
+    printf("\nEscolha o numero do territorio atacante: ");
+    scanf("%d", &a);
+
+    printf("Escolha o numero do territorio defensor: ");
+    scanf("%d", &d);
+
+    if (a == d) {
+        printf("Um territorio nao pode atacar a si mesmo!\n");
+    } else if (strcmp(mapa[a - 1].cor, mapa[d - 1].cor) == 0) {
+        printf("Voce nao pode atacar um territorio da mesma cor!\n");
+    } else {
+        atacar(&mapa[a - 1], &mapa[d - 1]);
     }
+
+    exibirTerritorios(mapa, qtd);
+
+    liberarMemoria(mapa);
 
     return 0;
 }
